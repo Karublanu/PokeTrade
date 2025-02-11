2e9ac4d9b0d7d0e61d355e8d40fdb4abc03f6b0a
burak <mehmetburakunal92@gmail.com>
2025-02-11 14:12:52 +0100

    set up Project

---
+++ PokeTrade/PokeTradeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokeTradeApp.swift
+//  PokeTrade
+//
+//  Created by Mehmet Burak Ünal on 11.02.25.
+//
+
+import SwiftUI
+
+@main
+struct PokeTradeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
